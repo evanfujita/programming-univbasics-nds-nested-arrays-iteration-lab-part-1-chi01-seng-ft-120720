3ec10a70ec59fd377d6879840f3dff90341d1317
Evan Fujita <evanfujita@gmail.com>
2020-11-05 19:55:41 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,10 +6,13 @@ def find_even_values(array)
   while count < array.length do
     inner_count = 0 
     while inner_count < array[count].length do
-      array[count][inner_count].even?
+      
+      if array[count][inner_count].even?
       puts array[count][inner_count]
+      end
+      
       inner_count += 1 
       end
-    end  
     count += 1
+end
 end
