613315f1f4e534112a94416203847b8faa4ba8bc
Evan <evanfujita@gmail.com>
2020-12-16 21:41:01 -0600

    commit tree bfa6f8621eec16ec2872a99c9ba4b4e7dd54748b

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,18 +1,15 @@
-array = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+require 'pry'
 
 def find_even_values(array)
-  count = 0
+  array.each do |array|
+    array.select do|number| 
+      if number.even? 
+        puts number
+      end
+  end
+end
   
-  while count < array.length do
-    inner_count = 0 
-    while inner_count < array[count].length do
-      
-      if array[count][inner_count].even?
-      puts array[count][inner_count]
-      end
-      
-      inner_count += 1 
-      end
-    count += 1
 end
-end
+
+
+
